64c851f3010287214f94c6b2dbb4d4b1d41ec131
TheNoZER0 <hypersonic3692@gmail.com>
2026-05-21 15:02:50 +1000

    auth: use CDN Clerk loader without proxy

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -14,9 +14,5 @@ export default clerkMiddleware(async (auth, req) => {
 });
 
 export const config = {
-  matcher: [
-    '/((?!_next|.*\\..*).*)',
-    '/(api|trpc)(.*)',
-    '/__clerk/(.*)',
-  ],
+  matcher: ['/((?!_next|.*\\..*).*)', '/(api|trpc)(.*)'],
 };
